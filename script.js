ee36b9c2f241fba70954fd101150b2d5c806b34f
JJackelope <al.zver95@gmail.com>
2025-08-14 12:56:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 javascript
 document.getElementById('add-note-btn').addEventListener('click',function () {
-const noteInput = document.getElementById('note-input');
-const notesContainer = document.getElementById('notes-container');
+    const noteInput = document.getElementById('note-input');
+    const notesContainer = document.getElementById('notes-container');
 
-if (noteInput.value.trim() === ") {
-alert('Пожалуйста, введи текст заметки.');
-return;
-}
+   if (noteInput.value.trim() === "") {
+       alert('Пожалуйста, введи текст заметки.');
+       return;
+    }
 
 const noteDiv = document.createElement('div');
-noteDiv.classList.add('note');
-noteDiv.textContent = noteInput.value;
+  noteDiv.classList.add('note');
+  noteDiv.textContent = noteInput.value;
 
-notesContainer.appendChild(noteDiv);
-noteInput.value = ";
+  notesContainer.appendChild(noteDiv);
+  noteInput.value = "";
 });
